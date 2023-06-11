6ad1456bdc05f07b1e7bf22cddfe5dc0efaa1fdf
DBARMPXKOS <dbarmpakos@outlook.com>
2023-06-12 00:45:28 +0300

    Add first firmware version

---
+++ firmware/wpp_v1.0/wpp_v1.0.ino
@@ -0,0 +1,21 @@
+#define FREQUENCY 117000
+hw_timer_t * timerX = NULL;
+volatile bool squareState = false;
+
+void IRAM_ATTR tim_callback() {
+  squareState = !squareState;
+}
+
+void 
+setup() {
+  timerX = timerBegin(0, 80, true);
+  timerAttachInterrupt(timerX, &tim_callback, true);
+  timerAlarmWrite(timerX, 1000000 / FREQUENCY/2, true);
+  timerAlarmEnable(timerX);
+}
+
+void 
+loop() {
+  if (squareState) { digitalWrite(FETPIN, HIGH); } 
+  else             { digitalWrite(FETPIN, LOW);  }         
+}
